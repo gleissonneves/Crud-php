3077862dadb1febeca1f0adff448cb386d92b969
Gleisson Neves <53718407+gdk46@users.noreply.github.com>
2021-01-13 20:28:30 -0300

    Documentação

--- autoload.php
+++ autoload.php
@@ -1,21 +1,37 @@
 <?php
 
 /**
- * AUTOLOAD DE CLASSES DENTRO DO PACOTE 'Class'
- * 
- * @param $classe
+ * Implementação específica do projeto.
+ *
+ * @param string $ class O nome totalmente qualificado da classe.
+ * @return void
  */
-function autoload($classe)
+
+function autoload($class)
 {
-    $diretorioBase = DIR_APP . DS;
-    $classe = $diretorioBase . 'Class' . DS . str_replace('\\', DS, $classe) . '.php';
-    
-    // debugger
-    // echo $classe."<br/>";
-    
-    
-    if (file_exists($classe) && !is_dir($classe)) {
-        include $classe;
+    // directórios especificos para requisição dos namespace
+    $baseDir = [
+        __DIR__ . DIRECTORY_SEPARATOR . 'Config' . $class . '.php',
+        __DIR__ . DIRECTORY_SEPARATOR . 'Database' . $class . '.php',
+    ];
+
+    // use a quantidade de item do array como referência, procure
+    for ($i = 0; $i < count($baseDir); $i++) {
+
+        // substitua \\ por / no index do directório base  
+        $class = str_replace('\\', DIRECTORY_SEPARATOR, $baseDir[$i]);
+
+        /**
+         * Debuggar caso seja necessário
+         * 
+         * $baseDir[$i] Mostra o caminho e a class 
+         */
+        // echo "<br/>".$baseDir[$i]."<br/>";
+
+        // se o arquivo existir e não for um directório, exija-o 
+        if (file_exists($class) && !is_dir($class)) {
+            include $class;
+        }
     }
 }
 
