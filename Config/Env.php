828f31b56f1dc029b4bbbcafa4cae63e66d2dea7
Gleisson Neves <53718407+gdk46@users.noreply.github.com>
2021-01-13 20:27:32 -0300

    arquivo de configuração de ambiente

---
+++ Config/Env.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Crud\Config;
+
+abstract class Env
+{
+    public static String $DB_CONNECTION = "mysql";
+    public static String $DB_HOST = "127.0.0.1";
+    public static String $DB_PORT = "3306";
+    public static String $DB_DATABASE = "test1";
+    public static String $DB_USERNAME = "root";
+    public static String $DB_PASSWORD = "";
+    
+}
+
